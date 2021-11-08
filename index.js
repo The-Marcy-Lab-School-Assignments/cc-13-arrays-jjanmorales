f02a70423cc796b65427283e7155026a9e045565
Jan Morales <moralesjan40@gmail.com>
2021-11-08 15:22:57 +0000

    Completed code challenge

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 //write your code here
+const averageInArray = numbers => {
+    let average = 0;
+    for(let i = 0;i < numbers.length; i++){
+        average += numbers[i];
+    }return average / numbers.length;
+};
+console.log(averageInArray([2,4,5,7,8]));
